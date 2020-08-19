17adc68cd433a5de8fa6de507ead6534cf51b687
Rich Pauloo <richpauloo@gmail.com>
2020-08-19 14:39:49 -0700

    change d to more informative s3dat and add error handling

--- R/get_data.R
+++ R/get_data.R
@@ -18,38 +18,43 @@
 #'   geom_point() +
 #'   geom_smooth()
 
+get_data <- function(psid = NULL, analyte = NULL, start_date = NULL, end_date = NULL) {
 
-get_data <- function(psid, start_date = NULL, end_date = NULL, analyte = NULL) {
+  # errors
+  if(is.null(psid)) stop("No psid was entered. To view a table of water systems, see the built-in object `water_systems`.", call. = FALSE)
 
-  if(! is.null(analyte)){
-    if(! (analyte %in% psid_analyte[which(psid_analyte$psid == psid), ]$analyte)){
-      stop(paste0("The requested analyte ", analyte,
-                  " is not found for the input psid ",
+  if(length(psid) > 1) stop(paste0("Only one psid can be entered, not ", length(psid), "."), call. = FALSE)
+
+  if(! psid %in% water_systems$psid) stop("Invalid psid. To view a table of water systems, see the built-in object `water_systems`.", call. = FALSE)
+
+  if(! class(psid) %in% c("character", "numeric", "integer")) {
+    stop("Argument `psid` must be of class `character`, `numeric`, or `integer`. To view a table of water systems, see the built-in object `water_systems`.", call. = FALSE)
+  }
+
+  if(! is.null(analyte)) {
+    if(length(analyte)>1) stop("Only a single analyte may be supplied.")
+    if(! (analyte %in% psid_analyte[which(psid_analyte$psid == psid), ]$analyte)) {
+      stop(paste0("The requested analyte '", analyte,
+                  "' is not found for the input psid ",
                   psid, ". Use `get_analyte_summary(",
                   psid, ")` to see available analytes."),
            call. = FALSE)
     }
   }
 
-  if(!is.character(psid)) {
-    stop("Argument `psid` must be of class `character`.
-         To view a table of water systems lookup table,
-         see the built-in object `water_systems`.",
-         call. = FALSE)
+  # construct url and ping s3
+  url <- paste0('https://sdwis.s3.us-west-1.amazonaws.com/', psid, '.csv.gz')
+  s3_dat <- data.table::fread(url)
+
+  # if present, filter by analyte, and start and end dates
+  if(! is.null(analyte)) {
+    s3_dat <- s3_dat[s3_dat$CHEMICAL == analyte, ]
   }
-
-  url = paste0('https://sdwis.s3.us-west-1.amazonaws.com/', psid, '.csv.gz')
-  s3_dat = data.table::fread(url)
-
-  # if present, filter by start and end dates and analytes
-  if(!is.null(start_date)){
+  if(! is.null(start_date)) {
     s3_dat <- s3_dat[s3_dat$SAMP_DATE >= start_date, ]
   }
-  if(!is.null(end_date)){
+  if(! is.null(end_date)) {
     s3_dat <- s3_dat[s3_dat$SAMP_DATE <= end_date, ]
-  }
-  if(!is.null(analyte)){
-    s3_dat <- s3_dat[s3_dat$CHEMICAL == analyte, ]
   }
   return(s3_dat)
 }
