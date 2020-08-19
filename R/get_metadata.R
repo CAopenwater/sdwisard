1921d62bff40cff653aebe046f200cbb43a2e816
ergz <erodriguez@flowwest.com>
2020-08-19 14:06:19 -0700

    removes instances of zipcode in metadata functions

--- R/get_metadata.R
+++ R/get_metadata.R
@@ -1,26 +1,15 @@
 #' Get Water System
-#' @description Enable user to discover water system ID when supplied a zipcode or county of interest
-#' @param zipcode zipcode
+#' @description Enable user to discover water system ID when supplied a county of interest
 #' @param county county
 #' @examples
-#' get_water_system(zipcode = "90201")
 #' get_water_system(county = "D")
 #' @export
-get_water_system <- function(...) {
+get_water_system <- function(county) {
 
-  arg <- list(...)
-  if (length(arg) == 0) stop("supply either a zipcode or county", call. = FALSE)
-  if (is.null(names(arg))) stop("supply named argument", call. = FALSE)
-  if (length(arg) > 1) stop("supply only zipcode or county", call. = FALSE)
-
-  if (names(arg) == "zipcode") {
-    water_system <- water_systems[water_systems$zipcode == arg$zipcode, ]$water_system_name
-  } else if (names(arg) == "county") {
-    water_system <- water_systems[water_systems$county == arg$county, ]$water_system_name
-  }
+  water_system <- water_systems[water_systems$county == county, ]$water_system_name
 
   if (!length(water_system)) {
-    warning(paste(names(arg), arg[[1]], "was not found"), call. = FALSE)
+    warning(paste("county", county, "was not found", call. = FALSE))
     water_system <- NA
   }
 
