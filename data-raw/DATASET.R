b439beb87bb6665ba29aa6cb805c5f2d5b7ca6b3
sadie <sgill@flowwest.com>
2020-08-19 09:22:30 -0700

    adds date and count columns to psid_analyte dummy data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,26 +1,30 @@
+library(tidyverse)
+
 # data.frame of colnames = c(psid, water_system_name, county, zipcode)
 water_systems <- tibble(psid = c("103039", "103040"),
                         water_system_name = c("A","B"),
                         county = c("D","E"),
                         zipcode = c(90201, 94601))
-usethis::use_data(water_systems, overwrite=TRUE)
+
+usethis::use_data(water_systems, overwrite = TRUE)
 
 # data.frame of colnames = c(storet, analytes)
 analytes <- tibble(storet  = c("00081", "00086"),
                    analyte = c("COLOR", "ODOR THRESHOLD @ 60 C"))
-usethis::use_data(analytes, overwrite=TRUE)
 
+usethis::use_data(analytes, overwrite = TRUE)
+
+# internal of colnames = c(psid, storet, analyte)
 files <- list.files("data-raw", pattern = ".csv", full.names = TRUE)
 psids <- str_remove_all(files, ".csv") %>% basename()
 
-
-# internal of colnames = c(psid, storet, analyte)
 psid_analyte <- purrr::map2_df(
   files, psids, ~ read_csv(.x) %>%
-  distinct(STORE_NUM, CHEMICAL) %>%
+  group_by(STORE_NUM, CHEMICAL) %>%
+  summarise(start_date = min(SAMP_DATE), end_date = max(SAMP_DATE), n = n()) %>%
   mutate(psid = .y)
   ) %>%
-  select(psid, storet = STORE_NUM, analyte = CHEMICAL)
+  select(psid, storet = STORE_NUM, analyte = CHEMICAL, start_date, end_date, n)
 
 
-usethis::use_data(psid_analyte, overwrite=TRUE)
+usethis::use_data(psid_analyte, overwrite = TRUE)
