ac79dcdb9ebf2ed26f0f7583ab5dfabae3293e7f
Rich Pauloo <richpauloo@gmail.com>
2020-10-03 14:36:13 -0700

    update help man, force class psid == character

--- R/get_data.R
+++ R/get_data.R
@@ -7,30 +7,29 @@
 #' @param storet A single storet to return.
 #' @return A data.table of storet results for the input PSID and date range.
 #' @examples
-#' d <- get_data("103039")
+#' d <- get_data("0105009")
 #' head(d)
 #'
 #' # Alameda Country Water District (psid = 110001)
 #' # nitrate data (storet = A-029)
-#' get_data(
-#'   psid   = "110001",
-#'   storet = "A-029"
-#' ) %>%
-#' ggplot(aes(SAMP_DATE, FINDING)) +
+#' d <- get_data(
+#'   psid   = "0110001",
+#'   storet = "A-029")
+#' ggplot(d, aes(SAMP_DATE, FINDING)) +
 #'   geom_point() +
 #'   geom_smooth()
 
 get_data <- function(psid = NULL, storet = NULL, start_date = NULL, end_date = NULL) {
 
   # errors in PSID
-  if(is.null(psid)) stop("No psid was entered. To find a water system PSID, query by county. For example: `get_water_system(county = 'Alameda')`.", call. = FALSE)
+  if(is.null(psid)) stop("No psid was entered.", call. = FALSE)
 
   if(length(psid) > 1) stop(paste0("Only one psid can be entered, not ", length(psid), "."), call. = FALSE)
 
-  if(! psid %in% water_systems$psid) stop("Invalid psid. To find a valid water system PSID, query by county. For example: `get_water_system(county = 'Alameda')`.", call. = FALSE)
+  if(! psid %in% water_systems$psid) stop("Invalid psid. Find a PSID by county, for example, get_water_system('Alameda').", call. = FALSE)
 
-  if(! class(psid) %in% c("character", "numeric", "integer")) {
-    stop("Argument `psid` must be of class `character`, `numeric`, or `integer`.", call. = FALSE)
+  if( class(psid) != "character") {
+    stop("Argument `psid` must be of class `character`.", call. = FALSE)
   }
 
   # errors in storet
@@ -41,15 +40,15 @@ get_data <- function(psid = NULL, storet = NULL, start_date = NULL, end_date = N
     if(! (storet %in% psid_analyte[which(psid_analyte$psid == psid), ]$storet)) {
       stop(paste0("The requested storet '", storet,
                   "' is not found for the input psid ",
-                  psid, ". Use `get_storet_summary(",
-                  psid, ")` to see available storets."),
+                  psid, ". Use get_storet_summary(",
+                  psid, ") to see available storets."),
            call. = FALSE)
     }
   }
 
   # construct url and ping s3
   url <- paste0('https://sdwis.s3.us-west-1.amazonaws.com/', psid, '.csv.gz')
-  s3_dat <- data.table::fread(url)
+  s3_dat <- data.table::fread(url, colClasses = c(PWSID = "character"))
 
   # if present, filter by storet, and start and end dates
   if(! is.null(storet)) {
